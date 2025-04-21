a2ce3f7409bc1a6924f8ab2ac72379e82d3a28aa
Nonso Chinwuba <nonsohchinwuba@gmail.com>
2025-04-21 18:12:27 +0100

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
           href={"/blogs"}
           className="bg-white text-sm text-blue-700 font-semibold py-2 px-6 rounded"
         >
-          Explore Blogs
+          Explore Blog
         </Link>
       </div>
     </div>
