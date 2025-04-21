9c33614b9d924689e9996106da89ac6a7cb26914
Nonso Chinwuba <nonsohchinwuba@gmail.com>
2025-04-21 18:34:14 +0100

    Adding Get All Blogs API Route

--- src/app/api/get-blogs/route.js
+++ src/app/api/get-blogs/route.js
@@ -1,3 +1,5 @@
+import connectToDB from "@/app/database";
+import Blog from "@/app/models/blogModel";
 import { NextResponse } from "next/server"
 
 
@@ -7,7 +9,21 @@ export async function GET() {
 
 
   try {
-    
+    await connectToDB()
+    const extractAllBlogsFromDatabase = await Blog.find({})
+
+    if(extractAllBlogsFromDatabase) {
+      return NextResponse.json({
+        success: true,
+        data: extractAllBlogsFromDatabase
+      })
+    } else {
+      
+      return NextResponse.json({
+        success: false,
+        message: "Something went wrong"
+      })
+    }
     
   } catch (error) {
     console.log(error);
