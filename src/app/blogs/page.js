30dc84d2b322d5971aabd9d3b637d15101c85b29
Nonso Chinwuba <nonsohchinwuba@gmail.com>
2025-04-21 19:44:40 +0100

    Creating The UI for Get All Blogs

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,9 +1,31 @@
 import BlogOverview from "@/components/blog-overview"
 
 
-function Blogs() {
+async function fetchListOfBlogs() {
+  try {
+    const apiResponse = await fetch('http://localhost:3000/api/get-blogs', {
+      method: "GET",
+      cache: "no-store"
+    })
+
+    const result = await apiResponse.json()
+
+    return result.data
+
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+async function Blogs() {
+
+  const blogLists = await fetchListOfBlogs()
+
+  // console.log(blogLists);
+  
+
   return (
-    <BlogOverview />
+    <BlogOverview blogLists={blogLists} />
   )
 }
 
--- src/components/blog-overview/index.js
+++ src/components/blog-overview/index.js
@@ -1,45 +1,59 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import AddNewBlog from "../add-new-blog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const initialBlogFormData = {
-  title : '',
-  description : ''
-}
+  title: "",
+  description: "",
+};
 
-function BlogOverview() {
+function BlogOverview({ blogLists }) {
   const [openBlogDialog, setOpenBlogDialog] = useState(false);
-  const [loading, setLoading] = useState(false)
-  const [blogFormData, setBlogFormData] = useState(initialBlogFormData)
+  const [loading, setLoading] = useState(false);
+  const [blogFormData, setBlogFormData] = useState(initialBlogFormData);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh()
+  }, [])
 
   async function handleSaveBlogData() {
     try {
-      setLoading(true)
+      setLoading(true);
       const apiResponse = await fetch("/api/add-blog", {
         method: "POST",
-        body: JSON.stringify(blogFormData)
-    });
+        body: JSON.stringify(blogFormData),
+      });
 
-    const result = await apiResponse.json()
+      const result = await apiResponse.json();
 
-    if(result.success) {
-      setBlogFormData(blogFormData)
-      setOpenBlogDialog(false)
-      setLoading(false)
-    }
+      if (result.success) {
+        setBlogFormData(blogFormData);
+        setOpenBlogDialog(false);
+        setLoading(false);
+        router.refresh()
+      }
 
-    console.log(result);
-
-
+      console.log(result);
     } catch (error) {
       console.log(error);
-      setLoading(false)
-      setBlogFormData(initialBlogFormData)
-      
+      setLoading(false);
+      setBlogFormData(initialBlogFormData);
     }
   }
-  
 
   return (
     <div className="flex flex-col gap-10 min-h-screen bg-gradient-to-r from-purple-500 to-blue-600 p-6">
@@ -52,7 +66,22 @@ function BlogOverview() {
         setBlogFormData={setBlogFormData}
         handleSaveBlogData={handleSaveBlogData}
       />
-      <div>Blog List Section</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
+        {blogLists && blogLists.length > 0
+          ? blogLists.map((blogItem) => (
+              <Card key={blogItem._id} className="p-5">
+                <CardContent>
+                  <CardTitle className="mb-5">{blogItem.title}</CardTitle>
+                  <CardDescription> {blogItem.description} </CardDescription>
+                  <div className="mt-5 flex gap-5  items-center">
+                    <Button>Edit</Button>
+                    <Button>Delete</Button>
+                  </div>
+                </CardContent>
+              </Card>
+            ))
+          : null}
+      </div>
     </div>
   );
 }
